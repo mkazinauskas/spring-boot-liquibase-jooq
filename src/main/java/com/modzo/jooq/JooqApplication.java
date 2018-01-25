5c4303ab848b2ee5f1061797885662490b30f75d
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-01-25 20:35:34 +0200

    Initial

---
+++ src/main/java/com/modzo/jooq/JooqApplication.java
@@ -0,0 +1,12 @@
+package com.modzo.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqApplication.class, args);
+	}
+}
